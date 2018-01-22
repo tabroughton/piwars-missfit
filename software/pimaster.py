b71b9aa051c8ced62bb2f5df959883e698a2703d
Tom. A. Broughton <tom.a.broughton@gmail.com>
2018-01-22 21:36:48 +0000

    pimaster with test harness ablock read not working having to use byte

--- software/pimaster.py
+++ software/pimaster.py
@@ -10,6 +10,8 @@
 import smbus
 bus = smbus.SMBus(1)
 
+import time
+
 # I2C Address of ATMEGA328-slave
 address = 0x04
 
@@ -25,45 +27,33 @@ unused = 0
 # retries before we decide we have IO Error through connection issue
 retries = 10
 
-debug = False
-# set debug mode (default off)
-def set_debugMode(on_off):
-    if on_off == 1 or on_off == 0:
-        debug = on_off;
-    if debug == True:
-        print ("debug mode on")
-
 # Write I2C block
 def write_i2c_block(address, block):
-	for i in range(retries):
-		try:
-			return bus.write_i2c_block_data(address, 1, block)
-		except IOError:
-			if debug:
-				print ("IOError")
-	return -1
+    for i in range(retries):
+        try:
+            return bus.write_i2c_block_data(address, 1, block)
+        except IOError:
+            print ("IOError")
+    return -1
 
 # Read I2C byte
 def read_i2c_byte(address):
-	for i in range(retries):
-		try:
-			return bus.read_byte(address)
-		except IOError:
-			if debug:
-				print ("IOError")
-	return -1
+    for i in range(retries):
+        try:
+            return bus.read_byte(address)
+        except IOError:
+            print ("IOError")
+    return -1
 
 
 # Read I2C block
 def read_i2c_block(address):
-	for i in range(retries):
-		try:
-			return bus.read_i2c_block_data(address, 1)
-		except IOError:
-			if debug:
-				print ("IOError")
-	return -1
-
+    for i in range(retries):
+        try:
+            return bus.read_i2c_block_data(address, 1)
+        except IOError:
+                print ("IOError")
+        return -1
 
 # Setting Up Pin mode on Arduino
 def pinMode(pin, mode):
@@ -76,22 +66,23 @@ def pinMode(pin, mode):
 # Arduino Digital Read
 def digitalRead(pin):
 	write_i2c_block(address, digitalRead_cmd + [pin, unused])
-	pinVal = read_i2c_byte(address)
-	return pinVal
+	digital_reading = read_i2c_byte(address)
+	return digital_reading
 
 # Arduino Digital Write
 def digitalWrite(pin, value):
 	write_i2c_block(address, digitalWrite_cmd + [pin, value])
 	return 1
 
-
 # Read analog value from Pin
 def analogRead(pin):
-	write_i2c_block(address, ananlogRead_cmd + [pin, unused])
-	read_i2c_byte(address)
-    # analog values may be greater than 255 and we are dealing with bytes
-	pinVal = read_i2c_block(address)
-	return pinVal[1] * 255 + pinVal[2]
+    write_i2c_block(address, analogRead_cmd + [pin, unused])
+    analog_reading =  read_i2c_byte(address)
+    return analog_reading
+    # there's an issue getting block data
+    # print read_i2c_block(address)
+    #return number
+    #return pinVal[1] * 255 + pinVal[2]
 
 # Write PWM
 def analogWrite(pin, value):
@@ -101,7 +92,6 @@ def analogWrite(pin, value):
 
 if __name__ == '__main__': # only run if main program
     print "testing pimaster"
-    set_debugMode(1)
     cont = 1
     while cont == 1:
         command = int(raw_input("what command index do you want? (6 for exit)"))
@@ -125,7 +115,12 @@ if __name__ == '__main__': # only run if main program
             digitalWrite(pin, val)
         elif command == 4:
             print "analog read"
-            print analogRead(pin, val)
+            try:
+                while True:
+                    print analogRead(pin)
+                    time.sleep(.05)
+            except KeyboardInterrupt:
+                pass
         elif command == 5:
             print "analog write"
             analogWrite(pin, val)
